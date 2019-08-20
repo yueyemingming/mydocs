f9a8c18452d7061cfa510c2d66d16b96a60bcb01
liruigang <yueyemingming@163.com>
2019-08-20 10:41:39 +0800

    -

--- java/springcloud/eg01/生产者和消费者/provider/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-drop table if exists my_user;
-CREATE TABLE my_user (
-  id int(11) NOT NULL,
-  user_age int(11) NOT NULL,
-  user_name varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
